006cf1e632a4090f820c925ba814c6d2bb8fd0d7
urban-bit <gabrielgabriela57@gmail.com>
2024-10-14 14:31:39 +0200

    Your commit message

--- movie_review_api/reviews/serializers.py
+++ movie_review_api/reviews/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -12,3 +13,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         if value < 1 or value > 5:
             raise serializers.ValidationError("Rating must be between 1 and 5.")
         return value
+
+class UserSerializer(serializers.ModelSerializer):
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'reviews']
