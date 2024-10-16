9fd1cd4b214fe0f11a93e3d892817a6e72107a68
urban-bit <gabrielgabriela57@gmail.com>
2024-10-16 10:00:57 +0200

    views update

--- movie_review_api/reviews/views.py
+++ movie_review_api/reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Review
+from .serializers import ReviewSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.contrib.auth.models import User
 
-# Create your views here.
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
