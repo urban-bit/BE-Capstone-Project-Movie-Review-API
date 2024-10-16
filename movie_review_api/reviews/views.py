729068aca0b65fa2793967c67f10d662f1e7d1f1
urban-bit <gabrielgabriela57@gmail.com>
2024-10-16 10:17:25 +0200

    viewser updates

--- movie_review_api/reviews/views.py
+++ movie_review_api/reviews/views.py
@@ -1,17 +1,32 @@
-from rest_framework import viewsets, permissions
+# reviews/views.py
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 from .models import Review
 from .serializers import ReviewSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django.contrib.auth.models import User
+from .permissions import IsOwnerOrReadOnly
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing review instances.
+    Allows users to create, update, and delete their own reviews.
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
+        """
+        Set the user to the current authenticated user when creating a new review.
+        """
         serializer.save(user=self.request.user)
 
+
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A viewset that provides read-only actions for User model.
+    Allows users to list all users or retrieve details of a specific user.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
